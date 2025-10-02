95ae1d3a390e70575c6e6665168842aa65c5822f
shuangmian <135198794+shuangmian-bai@users.noreply.github.com>
2025-10-02 23:46:08 +0800

    feat(movie): 添加电影数据响应类- 创建 MovieResponse 类用于封装电影数据- 添加 movie 字段及其 getter 和 setter 方法 - 实现基本的 POJO 结构以支持数据传输

---
+++ data_api/src/main/java/org/example/get_movie_data/controller/MovieResponse.java
@@ -0,0 +1,13 @@
+package org.example.get_movie_data.controller;
+
+public class MovieResponse {
+    private String movie;
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+}
